95686876e5ba46addd296bfea1038a3aa78083ac
Colin <colinnlsn@gmail.com>
2017-11-02 18:32:19 -0500

    Only broadcasting to a particular user

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/view/:id', function(req,res){
+app.get('/view/:id', function (req, res) {
     res.sendFile(__dirname + '/view.html');
 });
 
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
+    socket.on('ticker update', function (msg) {
+        //msg should be of form { id, update }
+        io.emit("ticker " + msg.id, msg.update);
+    })
 });
 
 http.listen(3000, function () {
