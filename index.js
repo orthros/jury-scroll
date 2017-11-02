9300d4939cef6f9af35401591d0a513f9ce04eb0
Colin <colinnlsn@gmail.com>
2017-11-02 16:50:43 -0500

    Added a route to index.js that sends users to the 'view' page

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/view/:id', function(req,res){
+    res.sendFile(__dirname + '/view.html');
+});
 
 io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
