34e3885b9131e242edc6b12d164a2565e3248b95
Colin <colinnlsn@gmail.com>
2017-11-02 20:35:58 -0500

    Added dotenv dependency & allow specifying ports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -19,6 +20,8 @@ io.on('connection', function (socket) {
     })
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+var port = process.env.PORT || 5000;
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
