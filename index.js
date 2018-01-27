9203890385b77cadea77a77c292419c24903b8c0
orthros <colinnlsn@gmail.com>
2018-01-27 14:58:22 -0600

    Initial commit of API to control the ticker Added a new get route /api/control/:id/:message to be able to control a ticker via an apior command in NightBot

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
@@ -14,7 +16,6 @@ app.get('/', function (req, res) {
 app.get('/control/:id', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
-
 
 app.get('/view/:id', function (req, res) {
     res.sendFile(__dirname + '/views/view.html');
@@ -25,6 +26,17 @@ io.on('connection', function (socket) {
         //msg should be of form { id, update }
         io.emit("ticker " + msg.id, msg.update);
     })
+});
+
+app.get('/api/control/:id/:message', function (req, res) {
+    if (req.params != undefined &&
+        req.params.id != undefined &&
+        req.params.message != undefined) {
+        var id = req.params.id;
+        var message = req.params.message;
+        io.emit("ticker " + id, message);
+    }
+    res.end();
 });
 
 var port = process.env.PORT || 5000;
